654113d9ca75a3d510c58dfaa3e90951b52c20f6
afshinrodgar <afshin.chichi@gmail.com>
2015-07-11 23:57:23 +0430

    some functions added to web interface

--- web_interface/akira.py
+++ web_interface/akira.py
@@ -4,6 +4,10 @@ from flask import Flask, request, session, \
                   redirect, url_for, render_template, flash
 from contextlib import closing
 
+#import control
+
+#control.init_board()
+
 # configuration
 DEBUG = True
 SECRET_KEY = b'-\x0e[\xda\xd7@\xbf\xdf\xef\xc2\xdcD#3E-u\xba\xbc\xac\xca\xf4\xd9\n'
@@ -39,6 +43,22 @@ def logout():
   session.pop('username', None)
   flash('You were logged out')
   return redirect(url_for('login'))
+
+@app.route('/lights', methods=['POST'])
+def lights():
+  if request.form['red'] == 'on':   GPIO.output(RED, 1)
+  else:                             GPIO.output(RED, 0)
+  if request.form['blue'] == 'on':  GPIO.output(BLUE, 1)
+  else:                             GPIO.output(BLUE, 0)
+  if request.form['green'] == 'on': GPIO.output(GREEN, 1)
+  else:                             GPIO.output(GREEN, 0)
+  return redirect(url_for('home'))
+
+@app.route('/message', methods=['POST'])
+def message():
+  if request.form['msg']:
+    lcd.message(request.form['msg'])
+  return redirect(url_for('home'))
 
 if __name__ == "__main__":
   app.run()
